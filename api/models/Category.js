29d34c807339b6b9ffd23ed72c7410a784b1da9b
rishad-sid <rishad.sid@gmail.com>
2021-08-21 18:19:19 +0600

    first blog commit

---
+++ api/models/Category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+//Category Model for Posts
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
